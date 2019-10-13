cfe8aaab0eb51f5a6dd205b50ae59143186ef9db
Alejandro Pacheco <alepacheco99@gmail.com>
2019-10-13 14:06:37 +0100

    Add api integration

--- App.tsx
+++ App.tsx
@@ -1,16 +1,18 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import axios from 'axios';
-
-
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import axios from "axios";
 
 export const App = () => {
-  const [message, setMessage] = useState('Loading');
+  const [message, setMessage] = useState("Loading");
 
-  axios.get('/api/').then((response: any) => {
-    setMessage(response);
-  })
-
+  axios
+    .get("/api/")
+    .then(({ data }) => {
+      setMessage(data);
+    })
+    .catch(err => {
+      setMessage("Api not workign.");
+    });
 
   return (
     <View style={styles.container}>
@@ -18,15 +20,15 @@ export const App = () => {
       <Text>{message}</Text>
     </View>
   );
-}
+};
 
 export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
--- README.md
+++ README.md
@@ -2,5 +2,19 @@
 
 
 ## Usage
+
 You need to add a zeit token to the repo secrets to deploy correctly when the CI runs.
 
+
+## To run locally (with api)
+
+This has the api and client code running simultaneously but does not support hot reloading on the client code.
+
+- `expo build:web`
+- `now dev`
+
+Loads on port 3000
+
+# To run locally (w/o api)
+
+- `expo start --web`
